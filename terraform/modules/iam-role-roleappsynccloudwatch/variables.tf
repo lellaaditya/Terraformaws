f3b95fe1d86958b505289b6c82d0e095c026cd02
Aditya Lella <139613275+lellaaditya@users.noreply.github.com>
2024-03-25 20:40:58 +0530

    Create variables.tf

---
+++ terraform/modules/iam-role-roleappsynccloudwatch/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "eu-west-1"
+}
+
+variable "account_id" {
+  default = "6"
+}
+
+variable "service_name" {
+  default = "content-service"
+}
+
+variable "service_environment" {
+  default = "dev"
+}
