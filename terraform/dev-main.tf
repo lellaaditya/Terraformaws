ea2b952a7346704ae0eb6e76b81abc4f3ca439e9
Aditya Lella <139613275+lellaaditya@users.noreply.github.com>
2024-03-25 20:15:56 +0530

    Create dev-main.tf

---
+++ terraform/dev-main.tf
@@ -0,0 +1,69 @@
+# RoleAppSyncCloudWatch
+module "aws_iam_role" {
+  source  = "./modules/iam-role"
+  
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AWSAppSyncPushToCloudWatchLogs"
+  ]
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "meppsync.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# BasicAppSyncPolicy
+module "aws_iam_policy" {
+  source  = "./modules/iam-role"
+
+  name        = "BasicAppSyncPolicy"
+  description = "Allows AppSync to access SSM parameters"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ssm:GetParameter",
+          "ssm:GetParameters"
+        ]
+        Resource = [
+          "arn:aws:ssm:${var.region}:${var.account_id}:parameter/${var.service_name}/${var.service_environment}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "BasicAppSyncPolicyAttachment" {
+  name        = "BasicAppSyncPolicy"
+  policy_arn = aws_iam_policy.BasicAppSyncPolicy.arn
+  roles      = [aws_iam_role.RoleAppSyncSSM.name]
+}
+
+# RoleAppSyncSSM
+module "aws_iam_role" {
+  source  = "./modules/iam-role"
+
+  ssume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "mepsync.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
